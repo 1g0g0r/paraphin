e54e70d366c35220883a34c6dd38e6346f48b794
Lazarev Igor <Lazarevigor.2001@mail.ru>
2024-06-20 22:31:19 +0300

    heat eq

--- paraphin/equations/calc_temperature.py
+++ paraphin/equations/calc_temperature.py
@@ -2,28 +2,24 @@ from paraphin.utils.constants import *
 from paraphin.utils.utils import up_ko, mid
 
 
-def calc_temperature(qp, R, m, m_0, S, S_0, Wp, Wp_0, Wps, p, k):
+def calc_temperature(T, m, m_0, S, C_o, C_w, C_f, p, k):
     """
     Вычисление концентрации взвешенных частиц парафина по явной схеме
     """
     @ti.kernel
-    def calc_particles_loop():
+    def calc_temperature_loop():
         for i in range(1, Nx - 1):
             for j in range(1, Ny - 1):
                 # записать цикл по граням
-                r1 = up_ko(k[i, j], S[i, j], p[i, j], k[i + 1, j], S[i + 1, j], p[i + 1, j]) * \
-                     mid(k[i, j], S[i, j], k[i + 1, j], S[i + 1, j]) * area * (p[i, j] - p[i + 1, j]) / hx
-                r2 = up_ko(k[i, j], S[i, j], p[i, j], k[i - 1, j], S[i - 1, j], p[i - 1, j]) * \
-                     mid(k[i, j], S[i, j], k[i - 1, j], S[i - 1, j]) * area * (p[i, j] - p[i - 1, j]) / hx
-                r3 = up_ko(k[i, j], S[i, j], p[i, j], k[i, j + 1], S[i, j + 1], p[i, j + 1]) * \
-                     mid(k[i, j], S[i, j], k[i, j + 1], S[i, j + 1]) * area * (p[i, j] - p[i, j + 1]) / hy
-                r4 = up_ko(k[i, j], S[i, j], p[i, j], k[i, j - 1], S[i, j - 1], p[i, j - 1]) * \
-                     mid(k[i, j], S[i, j], k[i, j - 1], S[i, j + 1]) * area * (p[i, j] - p[i, j - 1]) / hy
-                R[i, j] += dT / (m[i, j] * (1-S[i, j]) * ro_p * volume) * (-(R[i, j] * ro_p + ro_o * Wp[i, j]) *
+                for i1, j1, h in [(i+1, j, hx), (i-1, j, hx), (i, j+1, hy), (i, j-1, hy)]:
+                    r1 = up_ko(k[i, j], S[i, j], p[i, j], k[i + 1, j], S[i + 1, j], p[i + 1, j]) * \
+                         mid(k[i, j], S[i, j], k[i + 1, j], S[i + 1, j]) * area * (p[i, j] - p[i + 1, j]) / hx
+
+                T[i, j] += dT / (m[i, j] * S[i, j] * ro_w * C_w[i, j] + () * ro_p) / volume * (-(R[i, j] * ro_p + ro_o * Wp[i, j]) *
                             (m[i, j] * (1 - S[i, j]) - m_0[i, j] * (1 - S_0[i, j])) / dT - ro_o * Wp[i, j] *
                             (1 - S[i, j]) * (Wp[i, j] - Wp_0[i, j]) / dT - ro_p * qp * volume - ro_o *
                             (Wp[i, j] + Wps[i, j]) * (r1 + r2 + r3 + r4))
 
-    calc_particles_loop()
+    calc_temperature_loop()
 
     return R
