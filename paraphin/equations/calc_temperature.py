0de4d5090ba465706d2e41d672296aa00f790ad4
Lazarev Igor <Lazarevigor.2001@mail.ru>
2024-06-24 00:23:18 +0300

    add heat equation

--- paraphin/equations/calc_temperature.py
+++ paraphin/equations/calc_temperature.py
@@ -1,8 +1,8 @@
 from paraphin.utils.constants import *
-from paraphin.utils.utils import up_ko, mid
+from paraphin.utils.utils import up_kw, up_ko, mid
 
 
-def calc_temperature(T, m, m_0, S, C_o, C_w, C_f, p, k):
+def calc_temperature(T, m, m_0, S, C_o, C_w, C_f, C_p, R, Wps, p, k):
     """
     Вычисление концентрации взвешенных частиц парафина по явной схеме
     """
@@ -10,16 +10,25 @@ def calc_temperature(T, m, m_0, S, C_o, C_w, C_f, p, k):
     def calc_temperature_loop():
         for i in range(1, Nx - 1):
             for j in range(1, Ny - 1):
-                # записать цикл по граням
-                for i1, j1, h in [(i+1, j, hx), (i-1, j, hx), (i, j+1, hy), (i, j-1, hy)]:
-                    r1 = up_ko(k[i, j], S[i, j], p[i, j], k[i + 1, j], S[i + 1, j], p[i + 1, j]) * \
-                         mid(k[i, j], S[i, j], k[i + 1, j], S[i + 1, j]) * area * (p[i, j] - p[i + 1, j]) / hx
+                multiplier = dT / (m[i, j] * S[i, j] * ro_w * C_w[i, j] + m[i, j] * (1 - S[i, j]) * ro_o * C_o[i, j] +
+                                   (m * (1 - S[i, j]) * R + E_p) * ro_p * C_p + (1 - m - E_p) * ro_f * C_f) / volume
+                t1, t2, t3 = 0, 0, 0
+                # цикл по граням
+                for i1, j1, hij in [(i+1, j, hx), (i-1, j, hx), (i, j+1, hy), (i, j-1, hy)]:
+                    temp_val = mid(k[i, j], S[i, j], k[i1, j1], S[i1, j1]) * (p[i, j] - p[i1, j1]) / hij
 
-                T[i, j] += dT / (m[i, j] * S[i, j] * ro_w * C_w[i, j] + () * ro_p) / volume * (-(R[i, j] * ro_p + ro_o * Wp[i, j]) *
-                            (m[i, j] * (1 - S[i, j]) - m_0[i, j] * (1 - S_0[i, j])) / dT - ro_o * Wp[i, j] *
-                            (1 - S[i, j]) * (Wp[i, j] - Wp_0[i, j]) / dT - ro_p * qp * volume - ro_o *
-                            (Wp[i, j] + Wps[i, j]) * (r1 + r2 + r3 + r4))
+                    t1 += area * (T[i, j] - T[i1, j1]) / hij
+                    t2 += up_kw(k[i, j], S[i, j], p[i, j], k[i1, j1], S[i1, j1], p[i1, j1]) * temp_val
+
+                    t3 += up_ko(k[i, j], S[i, j], p[i, j], k[i1, j1], S[i1, j1], p[i1, j1]) * temp_val
+
+                t1 *= (m[i, j] * (S[i, j] * K_w + (1-S[i, j]) * K_o) +
+                       (m[i, j] * (1-S[i, j]) * R[i, j] + E_p) * K_p + (1 - m[i, j] - E_p) * K_f)
+
+                t2 *= T[i, j] * ro_w * C_w
+
+                t3 *= T[i, j] * (ro_o * C_o + ro_o * Wps * C_p) * area
 
     calc_temperature_loop()
 
-    return R
+    return T
